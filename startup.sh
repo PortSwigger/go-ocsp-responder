26908806d8eac4745927ee3af72defee32973151
Simon Baker <simon.baker@portswigger.net>
2023-06-21 08:17:16 +0100

    rework startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 PKCS11CONF=${DEPOT}/pkcs11-config.json
-PUBKEY=${DEPOT/}/servicekey.pub
+PUBKEY=${DEPOT}/servicekey.pub
+AWSKMSCONF=${DEPOT}/aws-kms-config.json
+
 #REGION=eu-west-1
 # things that should exist in parameter store manager and be available to us via env variables
 #SM_PKCS11_CONF=""     # an ARN of the config used by scepserver -pkcs11-config argument
@@ -18,21 +20,9 @@ getsecretblob() {
         echo grabbing $1 saving to $2
 }
 
-# First up, check to see if our pkcs11 config file exists, and grab it if not.
-if [ ! -f ${PKCS11CONF} ]; then
-        getsecretblob ${SM_PKCS11_CONF} ${PKCS11CONF}
-fi
-
-# next, we need to grab our aws-kms-pkcs11 config.
-# this test is redundant but makes it look pretty :-)
-if [ ! -f /etc/aws-kms-pkcs11/config.json ]; then
-        mkdir -p /etc/aws-kms-pkcs11/
-        getsecretblob ${SM_KMS_CONFIG} /etc/aws-kms-pkcs11/config.json
-fi
-
-if [ ! -f ${PUBKEY} ]; then
-        getsecretblob ${SM_PUBKEY} ${PUBKEY}
-fi
+getsecretblob ${SM_KMS_CONFIG} ${AWSKMSCONF}
+getsecretblob ${SM_PKCS11_CONF} ${PKCS11CONF}
+getsecretblob ${SM_PUBKEY} ${PUBKEY}
 
 # generate a CSR bassed on our key and name
 cat ${PKCS11CONF}
